11bc827d7a199952b55974877090d4180a128a61
Jeffrey Konowitch <jeff.konowitch@generalassemb.ly>
2013-10-15 10:01:28 -0400

    changes

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,12 @@ class ActorsController < ApplicationController
   def index
     @actors = Actor.all
   end
+
+  def show
+    @actor = Actor.find(params[:id])
+  end
+
+  def destroy
+    Actor.find(params[:id]).destroy
+  end
 end
